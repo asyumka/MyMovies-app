61bbb749f39eea509c04e5614bd2088ae369c563
Siumka Andriy <160485365+asyumka@users.noreply.github.com>
2026-02-06 11:55:46 +0200

    Refactor authentication functions in auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
-// 1. Получение временного токена
 export const createRequestToken = async () => {
   const response = await axios.get(`${BASE_URL}/authentication/token/new`, {
     params: { api_key: API_KEY },
@@ -11,7 +10,6 @@ export const createRequestToken = async () => {
   return response.data.request_token;
 };
 
-// 2. Обмен подтвержденного токена на Session ID
 export const createSessionId = async (requestToken) => {
   const response = await axios.post(
     `${BASE_URL}/authentication/session/new`,
@@ -21,7 +19,6 @@ export const createSessionId = async (requestToken) => {
   return response.data.session_id;
 };
 
-// 3. Получение данных аккаунта (чтобы узнать имя и ID пользователя)
 export const getAccountDetails = async (sessionId) => {
   const response = await axios.get(`${BASE_URL}/account`, {
     params: { api_key: API_KEY, session_id: sessionId },
@@ -29,7 +26,6 @@ export const getAccountDetails = async (sessionId) => {
   return response.data;
 };
 
-// 4. Удаление сессии (Logout)
 export const deleteSession = async (sessionId) => {
   await axios.delete(`${BASE_URL}/authentication/session`, {
     params: { api_key: API_KEY },
